fc2181b78b33c8520543c672a6d9b19393c193c6
amin kazemi <aminkazemideveloper@gmail.com>
2026-06-25 21:37:57 +0330

    [add] type for BoardCard component

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,38 @@ function App() {
         <main className={styles.main}>
           <ul className={styles.boards}>
             <li >
-                <BoardCard/>
+              <BoardCard
+                id={1}
+                color='blue'
+                description='in yek payam testi ast'
+                title='board 1'
+              />
             </li>
             <li >
-                <BoardCard/>
+              <BoardCard
+                id={2}
+                color='gray'
+                description='in yek payam testi ast in yek payam testi ast in yek payam testi ast in yek payam testi ast'
+                title='board 2'
+              />
             </li>
             <li >
-                <BoardCard/>
+              <BoardCard
+                id={3}
+                color='yellow'
+                description=' in yek payam testi ast in yek payam testi ast in yek payam testi ast in yek payam testi astin yek payam testi ast in yek payam testi ast in yek payam testi ast'
+                title='board 3'
+              />
             </li>
             <li >
-                <BoardCard/>
+              <BoardCard
+                id={4}
+                color='blue'
+                description=' in yek payam testi ast  in yek payam testi ast'
+                title='board 4'
+              />
             </li>
-          
+
           </ul>
         </main>
       </main>
--- src/components/boardCard/boardCard.tsx
+++ src/components/boardCard/boardCard.tsx
@@ -1,16 +1,30 @@
+import clsx from 'clsx'
 import styles from './boardCard.module.css'
+import type { ReactNode } from 'react'
 
-export default function BoardCard (){
+type Color = "gray" | "yellow" | "blue"
+
+
+type Props = {
+    id : number ;
+    title : string ;
+    description : string ;
+    color : Color
+}
+
+
+
+export default function BoardCard ({id,color ,title ,description}:Props): ReactNode{
     return (
-        <div className={styles.board}>
-                <div className={styles.cover + " yellow"}></div>
+        <div className={styles["board-card"]}>
+                <div className={clsx(styles.cover , color)}></div>
                 <div className={styles.content}>
                   <div className={styles.header}>
-                    <h3 className={styles.title}>Board 1</h3>
+                    <h3 className={styles.title}>{title}</h3>
                     <a href="#" className={styles.link}>View</a>
                   </div>
 
-                  <p className={styles.description}>amet consectetur adipisicing elit. Quis, dicta.</p>
+                  <p className={styles.description}>{description}</p>
 
                 </div>
               </div>
